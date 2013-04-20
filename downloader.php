d46b7008268c8cb62ca614c318504e7568700bfe
Niko Sams <niko.sams@gmail.com>
2013-04-20 22:20:19 +0200

    add form that allows choosing url to download

--- downloader.php
+++ downloader.php
@@ -1,6 +1,8 @@
 <?php
 if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] == '/ping') die('pong'); //for testing .htaccess functionality
 
+define('LOADING_GIF', "data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==");
+
 function exception_error_handler($errno, $errstr, $errfile, $errline ) {
     throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
 }
@@ -14,9 +16,9 @@ if (php_sapi_name() == 'cli') {
 $steps = array(
     'welcome' => 'StepWelcome',
     'check' => 'StepCheck',
+    'downloadApp' => 'StepDownloadApp',
     'downloadKwf' => 'StepDownloadKwf',
     'downloadLibrary' => 'StepDownloadLibrary',
-    'downloadApp' => 'StepDownloadApp',
     'extractKwf' => 'StepExtractKwf',
     'extractLibrary' => 'StepExtractLibrary',
     'extractApp' => 'StepExtractApp',
@@ -30,18 +32,19 @@ if (!isset($steps[$step])) throw new Exception("Invalid step");
 $stepClass = $steps[$step];
 $stepNum = array_search($step, array_keys($steps));
 $step = new $stepClass();
+echo "<div style=\"width: 800px; margin: 0 auto;\">\n";
 echo "<h1>Koala Framework Downloader</h1>";
 echo "<h2>$step->name [".($stepNum+1).'/'.count($steps)."]</h2>\n";
 $step->execute();
 
 $stepKeys = array_keys($steps);
-if (isset($stepKeys[$stepNum+1])) {
+if ($step->getShowNextStep() && isset($stepKeys[$stepNum+1])) {
     $nextStep = $stepKeys[$stepNum+1];
     $s = new $steps[$nextStep];
-    $loadingGif = "data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==";
-    $onClick = "this.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;'; this.style.backgroundImage = 'url($loadingGif)';";
+    $onClick = "this.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;'; this.style.backgroundImage = 'url(".LOADING_GIF.")';";
     echo "<p><a href=\"downloader.php?step=".$nextStep."\" onclick=\"$onClick\" style=\"background-repeat: no-repeat;\">Next Step: ".$s->name."</a></p>";
 }
+echo "</div>\n";
 
 //interesting way to do a http request, but file_get_contents might be blocked or something
 function httpRequestGet($url)
@@ -51,7 +54,8 @@ function httpRequestGet($url)
 
 abstract class Step
 {
-    abstract function execute();
+    abstract public function execute();
+    public function getShowNextStep() { return true; }
 }
 
 class StepWelcome extends Step
@@ -109,24 +113,52 @@ class StepCheck extends Step
             throw new Exception(".htaccess broken");
         }
         
-        echo "<p>All checks required for the downloader passed.</p>";
+        echo "<p>All checks required for the downloader passed.</p>\n";
+        echo "<p style=\"font-style:italic;\">Note: this doesn't include all requirements needed by kwf.</p>\n";
     }
 }
+
 abstract class StepDownload extends Step
 {
     protected $_url;
     protected $_target;
     public function execute()
     {
-        if (!file_exists($this->_target)) {
-            exec("wget -O $this->_target $this->_url", $out, $ret);
+        if (isset($_REQUEST['downloadUrl'])) {
+            $url = $_REQUEST['downloadUrl'];
+            exec("wget -O ".escapeshellarg($this->_target)." ".escapeshellarg($url), $out, $ret);
             if ($ret) {
-                throw new Exception("Download failed");
+                echo "<p>Download failed</p>";
             }
             echo "<p>Successfully Downloaded: $this->_target</p>";
-        } else {
-            echo "<p>Already Downloaded: $this->_target</p>";
         }
+        if (!file_exists($this->_target)) {
+            echo "<form action=\"downloader.php?step=".$_GET['step']."\" method=\"POST\">\n";
+            echo "<p></p>\n";
+            echo "<label for=\"appUrl\">Archive to download:</label><br />\n";
+            echo "<select id=\"predefUrls\" onchange=\"document.getElementById('downloadUrl').value=this.value;\">\n";
+            $predefinedUrls = $this->_getDownloadUrls();
+            foreach ($predefinedUrls as $k=>$r) {
+                echo "<option value=\"".htmlspecialchars($k)."\">$r</option>\n";
+            }
+            echo "<option value=\"\">own archive (url)</option>\n";
+            echo "</select><br />\n";
+            $urls = array_keys($predefinedUrls);
+            echo "<input style=\"width: 600px;\" type=\"text\" name=\"downloadUrl\" id=\"downloadUrl\" value=\"".$urls[0]."\"><br />\n";
+            $onClick = "this.parentNode.style.backgroundImage = 'url(".LOADING_GIF.")'; this.style.visibility = 'hidden';";
+            echo "<p style=\"background-repeat: no-repeat;\">";
+            echo "<input type=\"submit\" value=\"Download\" onclick=\"$onClick\" />\n";
+            echo "</p>";
+            echo "</form>\n";
+        } else {
+            echo "<p>Using: $this->_target</p>";
+        }
+    }
+    abstract protected function _getDownloadUrls();
+
+    public function getShowNextStep()
+    {
+        return file_exists($this->_target);
     }
 }
 abstract class StepExtract extends Step
@@ -174,23 +206,73 @@ abstract class StepExtract extends Step
         }
     }
 }
+class StepDownloadApp extends StepDownload
+{
+    public $name = 'Download App';
+    protected $_target = 'app.tar.gz';
+
+    public function execute()
+    {
+        echo "<p>Plase choose the application you want to install. This can be either
+                 a kwf demo application or your own. If the application code is hosted on github you can
+                 use the github archive functionality.<br />
+                 You can also upload $this->_target manually.</p>\n";
+        parent::execute();
+    }
+
+    protected function _getDownloadUrls()
+    {
+        $demoRepos = array();
+        $repos = json_decode(httpRequestGet('https://api.github.com/users/vivid-planet/repos'));
+        foreach ($repos as $repo) {
+            if (substr($repo->name, -5) == '-demo') {
+                $demoRepos['https://github.com/'.$repo->full_name.'/archive/master.tar.gz'] = $repo->name;
+            }
+        }
+        return $demoRepos;
+    }
+}
 class StepDownloadKwf extends StepDownload
 {
     public $name = 'Download Kwf';
-    protected $_url = 'https://github.com/vivid-planet/koala-framework/archive/3.3.tar.gz';
     protected $_target = 'kwf.tar.gz';
+
+    public function execute()
+    {
+        echo "<p>Plase choose the Koala Framework Version you want to install.<br />
+                 You can also upload $this->_target manually.</p>\n";
+        parent::execute();
+    }
+
+    protected function _getDownloadUrls()
+    {
+        $ret = array();
+        $kwfBranches = json_decode(httpRequestGet('https://api.github.com/repos/vivid-planet/koala-framework/branches'));
+        foreach ($kwfBranches as $b) {
+            $ret["https://github.com/vivid-planet/koala-framework/archive/$b->name.tar.gz"] = $b->name;
+        }
+        return $ret;
+    }
 }
 class StepDownloadLibrary extends StepDownload
 {
     public $name = 'Download Library';
-    protected $_url = 'https://github.com/vivid-planet/library/archive/master.tar.gz';
     protected $_target = 'library.tar.gz';
-}
-class StepDownloadApp extends StepDownload
-{
-    public $name = 'Download App';
-    protected $_url = 'https://github.com/vivid-planet/kwf-cms-demo/archive/master.tar.gz';
-    protected $_target = 'app.tar.gz';
+
+    public function execute()
+    {
+        echo "<p>Plase choose the library you want to install, usually default should suffice.<br />
+                 You can also upload $this->_target manually.</p>\n";
+        parent::execute();
+    }
+
+    protected function _getDownloadUrls()
+    {
+        $ret = array(
+            'https://github.com/vivid-planet/library/archive/master.tar.gz' => 'default'
+        );
+        return $ret;
+    }
 }
 class StepExtractKwf extends StepExtract
 {
@@ -224,7 +306,7 @@ class StepExtractApp extends StepExtract
         parent::execute();
         //TODO enable automatic deletion?
         //unlink("downloader.php");
-        echo "<p>download finished</p>";
+        echo "<p style=\"font-weight: bold;\">Congratulations, downloader finished!</p>";
         echo "<p><a href=\"/kwf/maintenance/setup\">start setup</a></p>";
     }
 }
